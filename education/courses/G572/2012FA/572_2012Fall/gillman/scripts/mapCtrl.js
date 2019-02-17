ea4b27421fb94d4558ab3342f9f7fcf72ec50de9
Timothy Prestby <tjprestby@gmail.com>
2019-02-17 15:36:48 -0600

    Finished Adding all objects for 2012 and 2013 fr G572

--- education/courses/G572/2012FA/572_2012Fall/gillman/scripts/mapCtrl.js
+++
@@ -1,9 +0,0 @@
-
-var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 14, 
-	center: new google.maps.LatLng(43.338261, -89.014379),
-	mapTypeId: google.maps.MapTypeId.ROADMAP,
-	minZoom:10,
-	panControl: false,
-	//mapTypeControl: false
-});
