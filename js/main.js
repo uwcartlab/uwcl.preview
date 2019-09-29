7d0fea6b51762aacb482bd04f4c6b24695263052
Timothy Prestby <tjprestby@gmail.com>
2019-09-28 22:07:10 -0500

    Updated styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-//wait for the page to load
-//select the table we want
-//run the DataTables function on it
 
 $(document).ready(function() {
-    $('#example').DataTable();
+    $(".carousel575").css({
+        'width': ($("#productionCarousel").width() + 'px')
+      });
+
 } );
 
-//DataTables adds ordering, searching, paging, and information to the table by default
