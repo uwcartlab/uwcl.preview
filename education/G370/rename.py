9ecba323b362bb47d9ecb7bb4600131e0f8c21d4
Timothy Prestby <tjprestby@gmail.com>
2019-12-18 22:23:59 -0600

    Added final project files

--- education/G370/rename.py
+++ education/G370/rename.py
@@ -4,14 +4,15 @@ import os
 # Function to rename multiple files
 def main():
     def removeYear(): 
-        for filename in os.listdir("C:/Users/Tim/Desktop/Workspace/Repos/uwcl.preview/education/G370/2013FA/PDF"):
+        for filename in os.listdir("C:/Users/Tim/Desktop/Workspace/uwcl.preview/education/G370/2019FA/PDF"):
             print(filename)
     def organizeLetters():
-        for filename in os.listdir("C:/Users/Tim/Desktop/Workspace/Repos/uwcl.preview/education/G370/2013FA/PDF"):
+        for filename in os.listdir("C:/Users/Tim/Desktop/Workspace/uwcl.preview/education/G370/2019FA/PDF"):
             i=1
-            name = filename[8:]
+            #Can change name to 8: if year is attached like 2010Fall
+            name = filename[0:]
             last = name[0]
-            #Skip First letter
+            #Skip First letter since name will have uppercase
             for letter in name[1:]:
                 if letter.isupper() == False:
                     last += letter
@@ -26,14 +27,15 @@ def main():
                     continue
                 elif letter == '.':
                     break
-            #os.rename('PDF/' + filename, 'PDF/' + 'Spring2016' + last + first +'.PDF')
-            print("<a href='2013FA/PDF/" + filename + "' " + "target='_blank'>")
+            #print(last + first)
+            #os.rename('2019FA/PDF/' + filename, '2019FA/PDF/' + 'Fall2019' + last + first +'.PDF')
+            print("<a href='2019FA/PDF/" + filename + "' " + "target='_blank'>")
             print("<div class='col-md-4 col-sm-6'>")
-            print("<img src='2013FA/ImgSq/" + filename[:-4] + ".png' " + "alt='...' class='final'>")
+            print("<img src='2019FA/ImgSq/" + filename[:-4] + ".png' " + "alt='...' class='final'>")
             print("<h3 class='finalProject'>        </h3>")
             print("<p><i>" + first + " " + last +"</i></p>")
             print("</div></a>")
-            print()
+            print
     #removeYear()       
     organizeLetters()
             
